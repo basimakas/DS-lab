b6cb16248ca81eba1344f699e406c800432d7a6e
basima kasim <basimakasim2k5@gmail.com>
2025-10-14 11:26:10 +0530

    Add files via upload

---
+++ program1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+int l,b,a,p;
+printf("enter length and breadth of a rectangle");
+scanf("%d%d",&l,&b);
+a=l*b;
+p=2*(l+b);
+printf("Area and perimeter of rectangle is %d %d /n",a,p);
+}
+
