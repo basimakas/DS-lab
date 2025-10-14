c8502d67405596af9112a83171b49fc485e9cf3d
basima kasim <basimakasim2k5@gmail.com>
2025-10-14 11:27:10 +0530

    Add files via upload

---
+++ program4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+int a;
+printf("enter the number");
+scanf("%d",&a);
+if(a%2==0){
+printf("given number is even");}
+else
+printf("given number is odd");
+}
