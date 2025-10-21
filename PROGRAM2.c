fe525a162f9bb6e947e4a84e772c8dbbe80cb69a
basima kasim <basimakasim2k5@gmail.com>
2025-10-21 11:40:49 +0530

    Add files via upload

---
+++ PROGRAM2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+int myfunction(int x, int y)
+{
+int c;
+c=x+y;
+return(c);
+}
+int main()
+{
+int a,b;
+printf("enter two numbers");
+scanf("%d%d",&a,&b);
+int result=myfunction(a,b);
+printf("result is%d",result);
+return 0;
+}
+
