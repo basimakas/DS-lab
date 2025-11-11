0561d0877bc38398dc43116a9a9b9a66c812e5e7
basima kasim <basimakasim2k5@gmail.com>
2025-11-11 12:50:31 +0530

    Add files via upload

---
+++ program5.c
@@ -0,0 +1,86 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+int data;
+struct node *left,*right;
+};
+struct node *insert(struct node*start,int data)
+{
+struct node*t;
+t=(struct node*)malloc(sizeof(struct node));
+t->data=data;
+t->left=(struct node*)0;
+t->right=start;
+if(start!=(struct node*)0)
+start->left=t;
+start=t;
+return start;
+}
+struct node *search(struct node*dl,int item)
+{
+while(dl!=(struct node*)0&&dl->data!=item)
+dl=dl->right;
+return dl;
+}
+struct node*delete(struct node*dl,struct node*t)
+{
+if(t->left==(struct node*)0 &&t->right==(struct node*)0)
+dl=(struct node*)0;
+else if(t->left==(struct node*)0)
+{
+t->right->left=(struct node*)0;
+dl=dl->right;
+}
+else if(t->right==(struct node*)0)
+t->left->right=(struct node*)0;
+else
+{
+t->left->right=t->right;
+t->right->left=t->left;
+}
+free(t);
+return dl;
+}
+void main()
+{
+struct node*l1=(struct node*)0,*t;
+int opt,data;
+do
+{
+printf("\n1.insert\n");
+printf("\n2.search\n");
+printf("\n3.delete\n");
+printf("\n4.exit\n");
+printf("\n5.option\n");
+scanf("%d",&opt);
+switch(opt)
+{
+case 1:printf("enter data:");
+       scanf("%d",&data);
+       l1=insert(l1,data);
+       printf("insertes %d",data);
+       break;
+case 2:printf("item to search:");
+       scanf("%d",&data);
+       l1=search(l1,data);
+       if(t==(struct node*)0)
+       printf("item is not found");
+       else
+       printf("item found");
+       break;      
+case 3:printf("item to delete:");
+       scanf("%d",&data);
+       t=search(l1,data);
+       if(t!=(struct node*)0)
+       {
+       l1=delete(l1,t);
+       printf("item deleted %d",data);
+       }
+       else
+       printf("Not exist\n");
+case 4:exit(0);
+}
+}
+while(1);
+}
